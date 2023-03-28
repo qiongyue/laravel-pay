a0cc8bc666e9f9837c87a508f46f7edceee49950
Jason <wangqiongyue@gmail.com>
2023-03-28 20:02:34 +0800

    m

---
+++ README.md
@@ -0,0 +1,74 @@
+<h1 align="center">Pay</h1>
+
+## 依赖
+
+- php >= 7.3 (v3.1.0 开始 >=7.4.0)
+- composer
+- laravel || lumen >= 8.0
+
+## 安装
+
+```Shell
+composer require yansongda/laravel-pay:~3.2.0
+```
+
+### laravel 用户
+
+#### 配置文件
+
+```Shell
+php artisan vendor:publish --provider="Yansongda\LaravelPay\PayServiceProvider" --tag=laravel-pay
+```
+
+### lumen 用户
+
+#### 配置文件
+
+请手动复制配置文件
+
+#### service provider
+
+```PHP
+$app->register(Yansongda\LaravelPay\PayServiceProvider::class);
+```
+
+## 使用方法
+
+### 支付宝
+
+```PHP
+use Pay;
+
+$order = [
+    'out_trade_no' => time(),
+    'total_amount' => '1',
+    'subject' => 'test subject - 测试',
+];
+
+return Pay::alipay()->web($order);
+
+// 下面这个方法也可以
+// return Pay::web($order);
+```
+
+### 微信
+
+```PHP
+use Pay;
+
+$order = [
+    'out_trade_no' => time(),
+    'body' => 'subject-测试',
+    'total_fee'      => '1',
+    'openid' => 'onkVf1FjWS5SBIixxxxxxxxx',
+];
+
+$result = Pay::wechat()->mp($order);
+
+```
+
+具体使用说明请传送至 [https://github.com/yansongda/pay](https://github.com/yansongda/pay)
+
+## License
+
+MIT
---
+++ src/PayServiceProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yansongda\LaravelPay;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
+use Yansongda\Pay\Pay;
+
+class PayServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Boot the service.
+     *
+     * @author yansongda <me@yansongda.cn>
+     */
+    public function boot()
+    {
+        if ($this->app instanceof Application && $this->app->runningInConsole()) {
+            $this->publishes([
+                dirname(__DIR__).'/config/pay.php' => config_path('pay.php'), ],
+                'laravel-pay'
+            );
+        }
+
+        if ($this->app instanceof LumenApplication) {
+            $this->app->configure('pay');
+        }
+    }
+
+    /**
+     * Register the service.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @throws \Yansongda\Pay\Exception\ContainerException
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(dirname(__DIR__).'/config/pay.php', 'pay');
+
+        Pay::config(config('pay'));
+
+        $this->app->singleton('pay.alipay', function () {
+            return Pay::alipay();
+        });
+
+        $this->app->singleton('pay.wechat', function () {
+            return Pay::wechat();
+        });
+
+        $this->app->singleton('pay.unipay', function () {
+            return Pay::unipay();
+        });
+    }
+
+    /**
+     * Get services.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['pay.alipay', 'pay.wechat', 'pay.unipay'];
+    }
+}
